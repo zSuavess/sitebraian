70d44393432c6d5a6774c1fcf662b2f2ed6d25a9
zSuavess <31328731+zSuavess@users.noreply.github.com>
2022-05-16 15:06:54 -0300

    Add files via upload

---
+++ games.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style.css">
+    <title>Games - FreeGames</title>
+</head>
+<body>
+
+    <div class="logo">
+                <a href="#"><img src="img/freegamesimagem.png" width="280px" /></a>
+    </div>
+
+    <nav>
+		<ul>
+            <li><a href="index.php">Início</a></li>
+			<li><a href="promocoes.php">Promoções</a></li>
+			<li><a class="active" href="games.php">Games</a></li>
+			<li><a href="sobrenos.php">Sobre nós</a></li>
+		</ul>
+	</nav>
+
+</body>
+</html>
---
+++ sobrenos.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style.css">
+    <title>Sobre nós - FreeGames</title>
+</head>
+<body>
+
+    <div class="logo">
+                <a href="#"><img src="img/freegamesimagem.png" width="280px" /></a>
+    </div>
+
+    <nav>
+		<ul>
+			<li><a href="index.php">Início</a></li>
+			<li><a href="promocoes.php">Promoções</a></li>
+			<li><a href="Games.php">Games</a></li>
+            <li><a class="active" href="#">Sobre nós</a></li>
+		</ul>
+	</nav>
+
+</body>
+</html>
